8b7e012c8baf30687d5f8a356595088c8692f3a3
tuvshinsmacbookpro <tuvshinsmacbookpro@localhost>
2025-11-25 10:19:49 +0000

    Update JavaApplication19.java

--- src/javaapplication19/JavaApplication19.java
+++ src/javaapplication19/JavaApplication19.java
@@ -15,6 +15,9 @@ public class JavaApplication19 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("sup nigga");
     }
+    
+  
     
 }
